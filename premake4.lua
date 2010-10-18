1dbe95857215a71a419878af0d860637de14d125
Bob Somers <bob@bobsomers.com>
2010-10-18 01:09:08 -0700

    Adding LuaJIT library for Windows and some Windows adjustments to the premake script.

--- premake4.lua
+++ premake4.lua
@@ -1,8 +1,9 @@
--- Premake script for generating build scripts
+-- premake script for generating build scripts
 -- see http://industriousone.com/premake for more info
 
 -- handy function for determining if we're on a 64-bit system (LINUX ONLY!)
 function sixtyFourBits()
+	if os.get() ~= "linux" then return false end
     local f = assert(io.popen("uname -m", "r"))
     local s = assert(f:read("*a"))
     f:close()
@@ -20,34 +21,35 @@ end
 solution "cudart"
     configurations { "Debug", "Release" }
     
-    -- Global debug settings
+    -- global debug settings
     configuration "Debug"
         defines { "DEBUG" }
         flags { "Symbols", "ExtraWarnings", "EnableSSE2", "NoPCH" }
         targetdir "bin/Debug"
         
-    -- Global release settings
+    -- global release settings
     configuration "Release"
         defines { "RELEASE", "NDEBUG" }
         flags { "OptimizeSpeed", "FloatFast", "ExtraWarnings", "EnableSSE2", "NoPCH" }
         targetdir "bin/Release"
         
-    -- Global Windows settings
+    -- global windows settings
     configuration "windows"
         defines { "WIN32", "_WIN32", "_USE_MATH_DEFINES" }
-        includedirs { "include/msinttypes" }
+        includedirs { "include/msinttypes", "c:/boost_1_44_0" }
     
-    -- Global Linux settings
+    -- global linux settings
     configuration "linux"
         defines { "LINUX" }
         includedirs { "/usr/local/cuda/include" }
+		links { "m", "dl" }
         if sixtyFourBits() then
             libdirs { "/usr/local/cuda/lib64" }
         else
             libdirs { "/usr/local/cuda/lib" }
         end
         
-    -- Haste project
+    -- haste project
     project "haste"
         kind "ConsoleApp"
         language "C++"
@@ -58,19 +60,19 @@ solution "cudart"
             "src/**.cpp"
         }
         links {
-            "m",
             "luajit",
             "luabind",
             "cudart"
         }
         uuid "56bdc40e-793c-4d8b-aeb0-ec1213fc391c"
 
-    -- LuaBind static library
+    -- luabind static library
     project "luabind"
         kind "StaticLib"
         language "C++"
         targetname "luabind"
         includedirs { "include" }
+		libdirs { "lib/" .. os.get() }
         files {
             "luabind/**.cpp"
         }
